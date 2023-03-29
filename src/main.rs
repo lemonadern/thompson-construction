7025ff62dd1553308f037947d6793d2b3ea82c0e
lemonadern <taishi.develop@gmail.com>
2023-03-29 23:32:46 +0900

    wip: main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use thompson_construction::expression_conversion::{infix_to_postfix, regex_to_infix};
+
 fn main() {
-    println!("abcdef");
+    let tokens: Vec<char> = "abc|(bc)+".chars().collect();
+
+    let tokens = regex_to_infix(&tokens);
+    let tokens = infix_to_postfix(&tokens);
+
+    // TODO: toNFA
+
+    // TODO: execute NFA
+
+    println!("{:?}", tokens);
 }
