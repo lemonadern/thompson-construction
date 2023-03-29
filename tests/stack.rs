59efe6d9e98423158f1f2a3a365d18fe5d24d8bc
lemonadern <taishi.develop@gmail.com>
2023-03-29 23:08:58 +0900

    feat: add stack.peek patterns

--- tests/stack.rs
+++ tests/stack.rs
@@ -6,14 +6,19 @@ use thompson_construction::stack::Stack;
 fn just_example() {
     let mut s: Stack<i32> = Stack::new();
 
+    assert_eq!(s.peek(), None);
     assert_eq!(s.pop(), None);
 
     s.push(1);
     s.push(3);
 
+    assert_eq!(s.peek(), Some(&3));
+    assert_eq!(s.peek(), Some(&3));
     assert_eq!(s.pop(), Some(3));
+
+    assert_eq!(s.peek(), Some(&1));
     assert_eq!(s.pop(), Some(1));
 
+    assert_eq!(s.peek(), None);
     assert_eq!(s.pop(), None);
-    println!("onegin");
 }
